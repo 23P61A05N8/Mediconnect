0d42be56739a34b8e6beca8403e2809b1a59bc99
23P61A05N8 <23p61a05n8@vbithyd.ac.in>
2026-03-24 09:18:52 +0530

    Update MediconnectInfo.java

--- MediconnectInfo.java
+++ MediconnectInfo.java
@@ -1,6 +1,4 @@
-public class MediconnectInfo {
-
-```
+public class MediconnectInfo{
 public static void main(String[] args) {
 
     System.out.println("===== MEDICONNECT PROJECT INFORMATION =====\n");
